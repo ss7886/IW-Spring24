3fbe11a4e02f5e35e363241606deccfce3eb1f32
Samuel Sanft (Linux) <sanft@princeton.edu>
2024-02-20 00:50:05 -0500

    started sparse matrix module

---
+++ jacobi/lib/sparse.ml
@@ -0,0 +1,66 @@
+module type SparseSig = sig
+  type matrix = {
+    n : int;
+    count : int;
+    vals : floatarray;
+    cols : int array;
+    row_ptr : int array;
+  }
+
+  val dense_to_sparse : floatarray array -> matrix
+
+  val get_val : matrix -> int -> int -> float
+  (* val set_val : matrix -> int -> int -> float -> unit
+
+  val mult_vec : matrix -> floatarray -> floatarray
+  val mult_row_vec : matrix -> int -> floatarray -> float *)
+end
+
+module SparseMatrix : SparseSig = struct
+  type matrix = {
+    n : int;
+    count : int;
+    vals : floatarray;
+    cols : int array;
+    row_ptr : int array;
+  } ;;
+
+  let dense_to_sparse (dense : floatarray array) : matrix = 
+    let n = Array.length dense in
+    let count_row (count : int) (row : floatarray) : int = 
+      Float.Array.fold_left (fun count x -> if x = 0. then count else count + 1) count row
+    in
+    let count = Array.fold_left (count_row) 0 dense in
+    let vals = Float.Array.create count in
+    let cols = Array.make count (-1) in
+    let row_ptr = Array.make n (-1) in
+    let index = ref 0 in
+    let populate (col : int) (x : float) : unit = 
+      if x = 0. then () else (
+      Float.Array.set vals !index x;
+      Array.set cols !index col;
+      index := !index + 1
+    ) in
+    let populate_row (rownum : int) (row : floatarray) : unit = (
+      Array.set row_ptr rownum !index;
+      Float.Array.iteri populate row
+    )
+    in 
+    let _ = Array.iteri populate_row dense in
+    {n=n; count=count; vals=vals; cols=cols; row_ptr=row_ptr}
+  
+  let get_val (m : matrix) (row : int) (col : int) : float = 
+    let n = m.n in
+    let count = m.count in
+    let vals = m.vals in
+    let cols = m.cols in
+    let row_ptr = m.row_ptr in 
+    let _ = assert (row < 0 || col < 0 || row >= n || col >= n) in
+    let start_index = Array.get row_ptr row in
+    let stop_index = if row < (n - 1) then Array.get row_ptr (row + 1) else count in
+    let rec find (index : int) (stop : int) : float =
+      if index = stop then 0. else
+        if Array.get cols index = col then Float.Array.get vals index else
+          find (index + 1) stop
+    in find start_index stop_index
+end
---
+++ jacobi/test/sparse.ml
@@ -0,0 +1,31 @@
+open Jacobi
+
+exception MismatchedVals
+
+let listlist_to_arrarr (list :  float list list) : floatarray array =
+  Array.of_list (List.map (Float.Array.of_list) list)
+
+let dense1 = listlist_to_arrarr [
+  [3.; 1.; 0.; 0.];
+  [2.; 2.5; 0.5; 0.];
+  [0.; 0.; 1.; -0.5];
+  [0.; 0.; 1.2; 2.0]
+]
+let dense2 = listlist_to_arrarr [
+  [1.; 0.; 0.; 0.];
+  [0.; 1.; 0.; 0.];
+  [0.; 0.; 1.; 0.];
+  [0.; 0.; 0.; 1.]
+]
+
+let sparse1 = Sparse.SparseMatrix.dense_to_sparse matrix1
+let sparse2 = Sparse.SparseMatrix.dense_to_sparse matrix2
+
+let test_sparse (dense : floatarray array) (sparse : Sparse.SparseMatrix.matrix) : unit =
+  let check_val (row : int) (col : int) (x : float) : unit = 
+    if x = Sparse.SparseMatrix.get_val sparse row col then () else raise MismatchedVals
+  in
+  Array.iteri (fun row_num row -> Float.Array.iteri (check_val row_num) row) dense
+
+let _ = test_sparse dense1 sparse1
+let _ = test_sparse dense2 sparse2
