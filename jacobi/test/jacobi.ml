eaa44db1c2b747ef8aa2576f8bd14c774348f49f
Samuel Sanft (Linux) <sanft@princeton.edu>
2024-02-27 05:04:33 -0500

    update testing

--- jacobi/test/jacobi.ml
+++ jacobi/test/jacobi.ml
@@ -1,4 +1,6 @@
 open JacobiLib
+open JacobiSeq
+open Sparse
 
 let listlist_to_arrarr (list :  float list list) : floatarray array =
   Array.of_list (List.map (Float.Array.of_list) list)
@@ -6,6 +8,20 @@ let listlist_to_arrarr (list :  float list list) : floatarray array =
 let print_vector (vec : floatarray) : unit = 
   Float.Array.iter (fun x' -> (print_float x'; print_string " ")) vec;
   print_newline ()
+
+(* let vec_eq (v1 : floatarray) (v2 : floatarray) : bool =
+  let rec aux (i : int) : bool =
+    if i = Float.Array.length(v1) then true else
+      let f1 = Float.Array.get v1 i in
+      let f2 = Float.Array.get v2 i in
+      if f1 = f2 then aux (i + 1) else (
+      print_float f1;
+      print_string " =/= ";
+      print_float f2;
+      print_newline ();
+      false
+    )
+  in aux 0 *)
 
 let matrix1 = listlist_to_arrarr [
   [3.; 1.; 0.; 0.];
@@ -34,28 +50,44 @@ let matrix3 = listlist_to_arrarr [
   [0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.; 0.95; 1.0];
 ]
 
+let sparse1 = SparseMatrix.dense_to_sparse matrix1
+let sparse2 = SparseMatrix.dense_to_sparse matrix2
+let sparse3 = SparseMatrix.dense_to_sparse matrix3
+
 let b1 = Float.Array.of_list [4.3; 3.2; 2.2; 3.1]
 let b2 = Float.Array.of_list [-2.2; 12.5; -0.2; 3.1]
 let b3 = Float.Array.of_list [0.0; 1.0; 2.0; 4.0]
 let b4 = Float.Array.of_list [-9.9; 8.8; -7.7; 6.6; -5.5; 4.4; -3.3; 2.2; -1.1; 0.05]
 
-let x1 = JacobiSeq.jacobi_seq matrix1 b1
+let x1 = jacobi_seq matrix1 b1
 let _ = (print_vector x1; print_newline ())
 
-let x2 = JacobiSeq.jacobi_seq matrix1 b2
+let x1_sparse = jacobi_sparse sparse1 b1
+let _ = (print_vector x1_sparse; print_newline ())
+(* let _ = assert (vec_eq x1 x1_sparse) *)
+
+let x2 = jacobi_seq matrix1 b2
 let _ = (print_vector x2; print_newline ())
 
-let x3 = JacobiSeq.jacobi_seq matrix1 b3
+let x3 = jacobi_seq matrix1 b3
 let _ = (print_vector x3; print_newline ())
 
-let x4 = JacobiSeq.jacobi_seq matrix2 b1
+let x4 = jacobi_seq matrix2 b1
 let _ = (print_vector x4; print_newline ())
 
-let x5 = JacobiSeq.jacobi_seq matrix2 b2
+let x4_sparse = jacobi_sparse sparse2 b1
+let _ = (print_vector x4_sparse; print_newline ())
+(* let _ = assert (vec_eq x4 x4_sparse) *)
+
+let x5 = jacobi_seq matrix2 b2
 let _ = (print_vector x5; print_newline ())
 
-let x6 = JacobiSeq.jacobi_seq matrix2 b3
+let x6 = jacobi_seq matrix2 b3
 let _ = (print_vector x6; print_newline ())
 
-let x7 = JacobiSeq.jacobi_seq matrix3 b4
+let x7 = jacobi_seq matrix3 b4
 let _ = (print_vector x7; print_newline ())
+
+let x7_sparse = jacobi_sparse sparse3 b4
+let _ = (print_vector x7_sparse; print_newline ())
+(* let _ = assert (vec_eq x7 x7_sparse) *)
--- jacobi/test/sparse.ml
+++ jacobi/test/sparse.ml
@@ -1,13 +1,30 @@
 open JacobiLib
+open JacobiSeq
+open Sparse
+open SparseUtil
 
 exception MismatchedVals
 
 let listlist_to_arrarr (list :  float list list) : floatarray array =
   Array.of_list (List.map (Float.Array.of_list) list)
 
-let print_vector (vec : floatarray) : unit = 
+(* let print_vector (vec : floatarray) : unit = 
   Float.Array.iter (fun x' -> (print_float x'; print_string " ")) vec;
-  print_newline ()
+  print_newline () *)
+
+let vec_eq (v1 : floatarray) (v2 : floatarray) : bool =
+  let rec aux (i : int) : bool =
+    if i = Float.Array.length(v1) then true else
+      let f1 = Float.Array.get v1 i in
+      let f2 = Float.Array.get v2 i in
+      if f1 = f2 then aux (i + 1) else (
+      print_float f1;
+      print_string " =/= ";
+      print_float f2;
+      print_newline ();
+      false
+    )
+  in aux 0
 
 let dense1 = listlist_to_arrarr [
   [3.; 1.; 0.; 0.];
@@ -22,19 +39,39 @@ let dense2 = listlist_to_arrarr [
   [0.; 0.; 0.; 1.]
 ]
 
-let sparse1 = Sparse.SparseMatrix.dense_to_sparse dense1
-let sparse2 = Sparse.SparseMatrix.dense_to_sparse dense2
+let b1 = Float.Array.of_list [4.3; 3.2; 2.2; 3.1]
+let b2 = Float.Array.of_list [-2.2; 12.5; -0.2; 3.1]
+let b3 = Float.Array.of_list [0.0; 1.0; 2.0; 4.0]
 
-let test_sparse (dense : floatarray array) (sparse : Sparse.SparseMatrix.matrix) : unit =
+let sparse1 = SparseMatrix.dense_to_sparse dense1
+let sparse2 = SparseMatrix.dense_to_sparse dense2
+
+let test_sparse (dense : floatarray array) (sparse : SparseMatrix.matrix) : unit =
   let check_val (row : int) (col : int) (x : float) : unit = 
-    if x = Sparse.SparseMatrix.get_val sparse row col then () else raise MismatchedVals
+    if x = SparseMatrix.get_val sparse row col then () else raise MismatchedVals
   in
   Array.iteri (fun row_num row -> Float.Array.iteri (check_val row_num) row) dense
 
 let _ = print_endline "Testing Sparse Matrices"
 
-let _ = print_vector sparse1.vals
-let _ = print_vector sparse2.vals
+(* let _ = print_vector sparse1.vals
+let _ = print_vector sparse2.vals *)
 
 let _ = test_sparse dense1 sparse1
 let _ = test_sparse dense2 sparse2
+
+let csv1 = read_sparse_from_csv "data/A-test1.csv"
+let csv2 = read_sparse_from_csv "data/A-test2.csv"
+
+let _ = assert (csv1 = sparse1)
+let _ = assert (csv2 = sparse2)
+
+(* let _ = print_vector (mat_vec_mult dense1 b1)
+let _ = print_vector (SparseMatrix.mult_vec sparse1 b1) *)
+
+(* let _ = assert (vec_eq (mat_vec_mult dense1 b1) (SparseMatrix.mult_vec sparse1 b1)) *)
+let _ = assert (vec_eq (mat_vec_mult dense1 b2) (SparseMatrix.mult_vec sparse1 b2))
+let _ = assert (vec_eq (mat_vec_mult dense1 b3) (SparseMatrix.mult_vec sparse1 b3))
+let _ = assert (vec_eq (mat_vec_mult dense2 b1) (SparseMatrix.mult_vec sparse2 b1))
+let _ = assert (vec_eq (mat_vec_mult dense2 b2) (SparseMatrix.mult_vec sparse2 b2))
+let _ = assert (vec_eq (mat_vec_mult dense2 b3) (SparseMatrix.mult_vec sparse2 b3))
