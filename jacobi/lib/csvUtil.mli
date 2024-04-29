c653d3e8e3f6f5cff61fdd0eaf6ee65de3ac7acc
Samuel Sanft (Linux) <sanft@princeton.edu>
2024-04-28 23:46:35 -0400

    final comment

--- jacobi/lib/csvUtil.mli
+++ jacobi/lib/csvUtil.mli
@@ -3,4 +3,7 @@ val read_sparse_from_csv : string -> Sparse.t
 
 val build_sparse_from_csv : string -> Sparse.t
 
+(*  build_sparse_from_mm file isSym builds a sparse matrix from a Matrix-Market
+ *  file in matrix coordinate real format. isSym represents if the matrix is
+ *  in symmetric format. *)
 val build_sparse_from_mm : string -> bool -> Sparse.t
